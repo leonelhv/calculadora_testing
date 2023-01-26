0bbf258e5ec3f076492e3ccaae68e25249ef0aa5
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-26 10:01:43 -0500

    add test unitarios

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,11 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-
+import { FormsModule } from '@angular/forms';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
+      imports: [FormsModule],
     }).compileComponents();
   });
 
@@ -26,6 +25,61 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('calculadora_testing app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'calculadora_testing app is running!'
+    );
+  });
+
+  it('should multiply 2 numbers', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    app.dato1 = '7';
+    app.dato2 = '8';
+    app.operacion = 'multiplicacion';
+    app.onOperacion();
+
+    const expectResult = 56;
+
+    expect(app.resultado).toEqual(expectResult);
+  });
+  it('should divide 2 numbers', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    app.dato1 = '18';
+    app.dato2 = '2';
+    app.operacion = 'division';
+    app.onOperacion();
+
+    const expectResult = 9;
+
+    expect(app.resultado).toEqual(expectResult);
+  });
+  it('should addition 2 numbers', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    app.dato1 = '7';
+    app.dato2 = '8';
+    app.operacion = 'suma';
+    app.onOperacion();
+
+    const expectResult = 15;
+
+    expect(app.resultado).toEqual(expectResult);
+  });
+  it('should subtract 2 numbers', () => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
+
+    app.dato1 = '17';
+    app.dato2 = '8';
+    app.operacion = 'resta';
+    app.onOperacion();
+
+    const expectResult = 9;
+
+    expect(app.resultado).toEqual(expectResult);
   });
 });
