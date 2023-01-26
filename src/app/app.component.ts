9545f147678eb04c57dddd516e0e55daa23b69f7
Leonel Huachaca <leonel.huachacav@gmail.com>
2023-01-26 08:57:04 -0500

    calculadora

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,37 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'calculadora_testing';
+
+  dato1: string = '';
+  dato2: string = '';
+  resultado = 0;
+
+  onOperacion(operacion: string) {
+    if (!operacion) return;
+
+    const dato1 = Number(this.dato1);
+    const dato2 = Number(this.dato2);
+
+    switch (operacion) {
+      case 'suma':
+        this.resultado = dato1 + dato2;
+        break;
+      case 'resta':
+        this.resultado = dato1 - dato2;
+        break;
+      case 'multiplicacion':
+        this.resultado = dato1 * dato2;
+        break;
+      case 'division':
+        this.resultado = dato1 / dato2;
+        break;
+
+      default:
+        break;
+    }
+  }
 }
